573c88517755c088338d1e2d9bbf53afe9218cc6
Lucien776 <Lucien776>
2022-01-24 13:36:24 +0100

    url

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostname: str
+    database_url: str
     database_port: str
     database_password: str
     database_name: str
